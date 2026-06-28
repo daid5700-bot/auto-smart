5336d7000678e7788c2ae07ec4711a25a750025f
Dai Do <doanhdaigr5.2004@gmail.com>
2026-06-29 01:00:54 +0700

    chore: remove compiled dist, cache folders and update .gitignore

--- dist/src/app/(auth)/layout.jsx
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = AuthLayout;
-function AuthLayout(_a) {
-    var children = _a.children;
-    return <>{children}</>;
-}
